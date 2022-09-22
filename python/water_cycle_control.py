d0cb703e6de42ff2c64a4cfca2b7fa1093623f39
markuspinter <markus.m.pinter@gmail.com>
2022-09-22 14:58:23 +0200

    update mapping

--- python/water_cycle_control.py
+++ python/water_cycle_control.py
@@ -1,5 +1,5 @@
-import state_controller;
-import event_manager;
+import _state_controller as state_controller;
+import _event_manager as event_manager;
 
 
 def main():
@@ -8,25 +8,25 @@ def main():
 	lastWaterPumps = -1;
 	currWaterPumps = -1;
 	
-	while (state_controller.GetStateValue("python_running") == 1):
-		currWaterValves = state_controller.GetStateValue("water_valves:sensor");
-		currWaterPumps = state_controller.GetStateValue("water_pumps:sensor");
+	while (state_controller.get_state_value("python_running") == 1):
+		currWaterValves = state_controller.get_state_value("water_valves:sensor");
+		currWaterPumps = state_controller.get_state_value("water_pumps:sensor");
 		if (currWaterValves != lastWaterValves or currWaterPumps != lastWaterPumps):
 			if (currWaterValves > 0):
-				state_controller.SetState("water_valves_return:sensor", 1);
-				state_controller.SetState("water_valves_out:sensor", 1);
+				state_controller.set_state("water_valves_return:sensor", 1);
+				state_controller.set_state("water_valves_out:sensor", 1);
 			else:
-				state_controller.SetState("water_valves_return:sensor", 0);
-				state_controller.SetState("water_valves_out:sensor", 0);
+				state_controller.set_state("water_valves_return:sensor", 0);
+				state_controller.set_state("water_valves_out:sensor", 0);
 			if (currWaterValves > 0 and currWaterPumps > 0):
-				state_controller.SetState("pump_cold_water:sensor", 0);
-				state_controller.SetState("pump_hot_water:sensor", 1);
+				state_controller.set_state("pump_cold_water:sensor", 0);
+				state_controller.set_state("pump_hot_water:sensor", 1);
 			elif (currWaterValves == 0 and currWaterPumps > 0):
-				state_controller.SetState("pump_cold_water:sensor", 1);
-				state_controller.SetState("pump_hot_water:sensor", 0);
+				state_controller.set_state("pump_cold_water:sensor", 1);
+				state_controller.set_state("pump_hot_water:sensor", 0);
 			elif (currWaterPumps == 0):
-				state_controller.SetState("pump_cold_water:sensor", 0);
-				state_controller.SetState("pump_hot_water:sensor", 0);
+				state_controller.set_state("pump_cold_water:sensor", 0);
+				state_controller.set_state("pump_hot_water:sensor", 0);
 			lastWaterValves = currWaterValves;
 			lastWaterPumps = currWaterPumps;
 			
